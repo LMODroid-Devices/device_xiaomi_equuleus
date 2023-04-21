a90372625d8579e61d909a896d2ad4ed9dec21fa
LMO GerritBot <gerrit@libremobileos.com>
2023-04-21 22:18:45 +0000

    Adapt to LMODroid by device-add script
    Change-Id: I31923e1724e226c465f124e829696f5977208533

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_equuleus.mk
+    $(LOCAL_DIR)/lmodroid_equuleus.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_equuleus-user \
-    lineage_equuleus-userdebug \
-    lineage_equuleus-eng
+    lmodroid_equuleus-user \
+    lmodroid_equuleus-userdebug \
+    lmodroid_equuleus-eng
